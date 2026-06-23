b21af726c1f18420abb422c94f455707874ee594
Guru107 <guruak107@gmail.com>
2026-06-23 12:40:30 +0530

    feat: add Gate Pass Invoice child doctype (#17)

---
+++ gate_entry/gate_entry/doctype/gate_pass_invoice/gate_pass_invoice.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Gurudatt Kulkarni and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class GatePassInvoice(Document):
+	pass
